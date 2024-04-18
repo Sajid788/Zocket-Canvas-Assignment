cd03f6d3ed412789111a5f15fc461e6aae1004e8
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-04-18 15:18:36 +0530

    Implement the template data

--- canvas/src/components/Canvas.jsx
+++ canvas/src/components/Canvas.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 
-const Canvas = () => {
+const Canvas = (...props) => {
+    const templateData = {
+        caption: {
+          text: props.content || "1 & 2 BHK Luxury Apartments at just Rs.34.97 Lakhs",
+          position: { x: 50, y: 100 },
+          font_size: 44,
+          alignment: "left",
+          text_color: "#FFFFFF",
+          max_characters_per_line: 31,
+        },
+        cta: {
+          text: props.ctaText || "Shop Now",
+          position: { x: 190, y: 320 },
+          font_size: 30,
+          text_color: "#FFFFFF",
+          background_color: "#000000",
+          wrap_length: 200,
+        },
+        image_mask: {
+          x: 56,
+          y: 442,
+          width: 970,
+          height: 600,
+        },
+        urls: {
+          mask: "https://d273i1jagfl543.cloudfront.net/templates/global_temp_landscape_temp_10_mask.png",
+          stroke:
+            "https://d273i1jagfl543.cloudfront.net/templates/global_temp_landscape_temp_10_Mask_stroke.png",
+          design_pattern:
+            "https://d273i1jagfl543.cloudfront.net/templates/global_temp_landscape_temp_10_Design_Pattern.png",
+        },
+      };
 
     return (
     <>
