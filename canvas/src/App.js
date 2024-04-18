5515a5ed0d1cc1dbb11165863af3ad08ec8a7f83
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-04-18 19:25:54 +0530

    Implement the all logic

--- canvas/src/App.js
+++ canvas/src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-import Canvas from './components/Canvas';
 import Editor from './components/Editor';
 
 function App() {
--- canvas/src/components/Editor.jsx
+++ canvas/src/components/Editor.jsx
@@ -1,12 +1,38 @@
 import { CiImageOn } from "react-icons/ci";
+
 import Canvas from "./Canvas";
+import { useState } from "react";
 
 const Editor = () => {
- 
+  const [color,setColor] = useState("#0369A1")
+  const [content, setContent] = useState("")
+  const [ctaText, setCtaText] = useState("")
+  const [imgSrc, setImgSrc] = useState("")
+  const [pickedColors, setPickedColors] = useState(["#0369A1"])
+  
+  console.log(pickedColors)
+  const handleCtaChange = (event) => {
+    const text = event.target.value
+    setCtaText(text)
+  }
+  const handleAdContentChange = (event) => {
+    const text = event.target.value
+    setContent(text)
+  }
+  const handleImageChange = (event) =>{
+    const url = URL.createObjectURL(event.target.files[0])
+    setImgSrc(url)
+  }
+
+  const handleColorChange = (event) => {
+    const selectedColor = event.target.value;
+    setColor(selectedColor)
+    setPickedColors((prev)=>[...prev, selectedColor])
+  };
   return (
     <>
       <div className="flex flex-row">
-        <Canvas  />
+        <Canvas content={content} color={color} img={imgSrc} ctaText={ctaText} />
         <div className="flex flex-col items-center w-3/4 h-screen justify-center">
           <p className="font-bold text-xl">Ad customization</p>
           <p className="text-xs text-gray-500">
@@ -27,7 +53,7 @@ const Editor = () => {
               id="file-input"
               type="file"
               style={{ display: "none" }}
-              
+              onChange={handleImageChange}
             ></input>
           </div>
           <div className="flex flex-row w-4/5 items-center justify-center mt-4">
@@ -36,11 +62,11 @@ const Editor = () => {
             <div className="border h-0 w-80 ml-2"></div>
           </div>
           <div className="flex flex-col border rounded-xl w-4/5 my-2 p-2 px-4">
-            <p className="text-xs text-gray-400">Ad Content</p>
+            <p className="text-xs text-gray-400 ml-16">Ad Content</p>
             <input
               type="text"
               className="border-none focus:border-none focus:outline-none"
-              
+              onChange={handleAdContentChange}
             ></input>
           </div>
           <div className="flex flex-col border rounded-xl w-4/5 my-2 p-2 px-4">
@@ -48,12 +74,26 @@ const Editor = () => {
             <input
               type="text"
               className="border-none focus:border-none focus:outline-none"
-              
+              onChange={handleCtaChange}
             ></input>
           </div>
           <div className="flex flex-col w-4/5">
             <p className="text-sm text-gray-400">Choose your color</p>
             <div className="flex flex-row">
+              {
+                pickedColors.slice(-5).map((item,index)=>(
+                  <div
+                  key={index}
+                  className="h-8 w-8 rounded-full m-1"
+                  onClick={()=>setColor(item)}
+                  style={{                  
+                    backgroundColor: item
+                  }}/>
+                ))
+              }
+              <input type="color"
+              value={color} 
+              onChange={handleColorChange}></input>
             </div>
           </div>
         </div>
